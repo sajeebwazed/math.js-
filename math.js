fb5ffc5eade095293ff61eac01c2555bf404f522
Sajeeb Wazed <sojibwazedshuvo188@gmail.com>
2020-12-18 04:12:12 +0600

    created a new file math.js date.js

---
+++ math.js
@@ -0,0 +1,17 @@
+console.log(Math.E)
+console.log(Math.PI)
+
+var n = 4.567
+
+console.log(Math.abs(n))
+console.log(Math.floor(n))
+console.log(Math.ceil(n))
+console.log(Math.sqrt(64))
+console.log(Math.sqrt(98))
+console.log(Math.max(450,560,888))
+console.log(Math.min(450,560,888))
+console.log(Math.round(n))
+console.log(Math.round(Math.random() * 51 + 1))
+console.log(Math.pow(3,5))
+console.log(Math.pow(8,9))
+
